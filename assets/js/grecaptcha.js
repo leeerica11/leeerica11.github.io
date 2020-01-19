e744a302126fa5dcb828ae1250b45814738f7aa4
Evan Zhao <etz3@cornell.edu>
2020-01-18 22:53:27 -0800

    Revert "temp"
    This reverts commit 6b65a9000dbafc8c1ed967fc136c4964e420476a.

--- assets/js/grecaptcha.js
+++
@@ -1,8 +0,0 @@
-var captchaOnloadCallback = function() {
-    // We only have one captcha object on this website, so no
-    // need to store the captcha object
-    grecaptcha.render(document.getElementById('killbot'), {
-        'sitekey' : '6LdHp9AUAAAAAAe1AhKIdKWmucBZ3lwfgy4Ts4m9',
-        'theme' : 'light'
-    });
-};
